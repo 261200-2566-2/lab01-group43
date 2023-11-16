e49683a20715222a9543de618fb904dc9f37dd1d
Punnawich Mai <punnawich_dachin@gmail.com>
2023-11-16 19:16:26 +0700

    Finished Lab01

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) { // class main ในการสร้างตัวแปรๆต่าง
+        Scanner s = new Scanner(System.in); // สร้าง scanner ขึ้นมา
+        System.out.print("Enter number : ");
+        int num = s.nextInt(); // ให้ num มารับเลขที่ input จะที่เราพิมพ์ (s.nextInt)
+        int[] arr = new int[num]; // สร้าง array จากเลขที่ input เข้ามา
+        System.out.print("Pls enter number what you want : ");
+        for (int i = 0; i < num; i++) {
+            arr[i] = s.nextInt(); // วน for เอาค่าที่ input เข้าไปใน array แต่ละตัวตามจำนวนขนาด array
+        }
+        sort(arr); // ส่งไปยัง method sort
+    }
+    public static void sort(int[] arr){ // เรียงข้อมูลใน array โดยรับ array และ size of array มา
+        Arrays.sort(arr); // ใช้ import class array เพื่อใช้คำสั่ง sort array
+        System.out.println("Sorted Array is ");
+        for(int i : arr){
+            System.out.println(i); // print element in array
         }
     }
+
 }
+
